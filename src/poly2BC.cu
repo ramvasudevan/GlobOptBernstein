28724a4b104d1ff0ac60e1f30e64d55c3efe8518
Cfather <41474684+Cfather@users.noreply.github.com>
2019-11-30 22:40:05 -0500

    Add files via upload

---
+++ src/poly2BC.cu
@@ -0,0 +1,160 @@
+# ifndef POLY_2_BC_CPP_INCLUDED
+# define POLY_2_BC_CPP_INCLUDED
+# include "poly2BC.h"
+
+poly::poly()
+{
+	numDimension = 0;
+	numTerms = 0;
+	coeff = NULL;
+	degree = NULL;
+	maxDegree = NULL;
+}
+
+poly::~poly()
+{
+	delete[] coeff;
+	delete[] degree;
+	delete[] maxDegree;
+}
+
+poly::poly(uint8_t numDimension_input, uint16_t numTerms_input, double* data_mat)
+{
+	numDimension = numDimension_input;
+	numTerms = numTerms_input;
+
+	coeff = new float[numTerms];
+	for (uint16_t i = 0; i < numTerms; i++)
+	{
+		coeff[i] = (float)data_mat[i * (numDimension + 1) + numDimension];
+	}
+
+	degree = new uint8_t[numTerms * numDimension];
+	maxDegree = new uint8_t[numDimension];
+	for (uint16_t i = 0; i < numDimension; i++)
+	{
+		maxDegree[i] = 0;
+		for (uint16_t j = 0; j < numTerms; j++)
+		{
+			degree[j * numDimension + i] = (uint8_t)data_mat[j * (numDimension + 1) + i];
+			if (degree[j * numDimension + i] > maxDegree[i])
+			{
+				maxDegree[i] = degree[j * numDimension + i];
+			}
+		}
+		maxDegree[i] += 1;
+	}
+}
+
+poly::poly(uint8_t numDimension_input, uint16_t numTerms_input, float* data_mat)
+{
+	numDimension = numDimension_input;
+	numTerms = numTerms_input;
+
+	coeff = new float[numTerms];
+	for (uint16_t i = 0; i < numTerms; i++)
+	{
+		coeff[i] = data_mat[i * (numDimension + 1) + numDimension];
+	}
+
+	degree = new uint8_t[numTerms * numDimension];
+	maxDegree = new uint8_t[numDimension];
+	for (uint16_t i = 0; i < numDimension; i++)
+	{
+		maxDegree[i] = 0;
+		for (uint16_t j = 0; j < numTerms; j++)
+		{
+			degree[j * numDimension + i] = (uint8_t)data_mat[j * (numDimension + 1) + i];
+			if (degree[j * numDimension + i] > maxDegree[i])
+			{
+				maxDegree[i] = degree[j * numDimension + i];
+			}
+		}
+		maxDegree[i] += 1;
+	}
+}
+
+poly::poly(uint8_t numDimension_input, uint16_t numTerms_input, double* degree_input, double* coef_input) {
+	numDimension = numDimension_input;
+	numTerms = numTerms_input;
+
+	coeff = new float[numTerms];
+	for (uint16_t i = 0; i < numTerms; i++)
+	{
+		coeff[i] = (float)coef_input[i];
+	}
+
+	degree = new uint8_t[numTerms * numDimension];
+	maxDegree = new uint8_t[numDimension];
+	for (uint16_t i = 0; i < numDimension; i++)
+	{
+		maxDegree[i] = 0;
+		for (uint16_t j = 0; j < numTerms; j++)
+		{
+			degree[j * numDimension + i] = (uint8_t)degree_input[j * numDimension + i];
+			if (degree[j * numDimension + i] > maxDegree[i])
+			{
+				maxDegree[i] = degree[j * numDimension + i];
+			}
+		}
+		maxDegree[i] += 1;
+	}
+}
+
+void poly::printDetails() {
+	mexPrintf("DIMENSION: %d, TERMS: %d\n", numDimension, numTerms);
+	for (uint16_t i = 0; i < numTerms; i++) {
+		mexPrintf("%.6f,", coeff[i]);
+		for (uint16_t j = 0; j < numDimension; j++) {
+			mexPrintf(" %d,", degree[i * numDimension + j]);
+		}
+		mexPrintf("\n");
+	}
+}
+
+void poly::partialDerivative(poly* &res, float &res_value, uint8_t dim) {
+	float* data_mat = new float[numTerms * (numDimension + 1)];
+	uint16_t pd_numTerms = 0;
+	float pd_value = 0;
+	for (uint16_t i = 0; i < numTerms; i++) {
+		if (degree[i * numDimension + dim] > 1) {
+			data_mat[pd_numTerms * (numDimension + 1) + numDimension] = coeff[i] * degree[i * numDimension + dim];
+
+			for (uint8_t j = 0; j < numDimension; j++) {
+				data_mat[pd_numTerms * (numDimension + 1) + j] = degree[i * numDimension + j];
+			}
+			data_mat[pd_numTerms * (numDimension + 1) + dim]--;
+
+			pd_numTerms++;
+		}
+		else if (degree[i * numDimension + dim] == 1) {
+			uint8_t temp = 0;
+			for (uint8_t j = 0; j < numDimension; j++) {
+				if (j != dim) {
+					temp += degree[i * numDimension + j];
+				}
+			}
+
+			if (temp > 0) {
+				data_mat[pd_numTerms * (numDimension + 1) + numDimension] = coeff[i] * degree[i * numDimension + dim];
+
+				for (uint8_t j = 0; j < numDimension; j++) {
+					data_mat[pd_numTerms * (numDimension + 1) + j] = degree[i * numDimension + j];
+				}
+				data_mat[pd_numTerms * (numDimension + 1) + dim]--;
+
+				pd_numTerms++;
+			}
+			else {
+				pd_value -= coeff[i];
+			}
+		}
+	}
+
+	res = new poly(numDimension, pd_numTerms, data_mat);
+	res_value = pd_value;
+
+	delete[] data_mat;
+}
+
+# endif
