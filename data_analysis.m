356add6d04a302cc07625c4c2e24fbf14bc04820
Cfather <michiganlol@sjtu.edu.cn>
2019-12-10 15:17:10 -0500

    Update data_analysis.m

--- data_analysis.m
+++ data_analysis.m
@@ -1,5 +1,26 @@
-%% optimal value analysis
-problem_index = 9;
+%% description
+% This script plots the data for the "increasing constraints" problems that
+% compare our PCBA algorithm, Lasserre's BSOS, and MATLAB's fmincon.
+%
+% The problem_index variable determines which problem to plot:
+%   1 - El-Attar-Vidyasagar-Dutta
+%   2 - Powell
+%   3 - Wood
+%   4 - Dixon-Price 2-D
+%   5 - Dixon-Price 3-D
+%   6 - Dixon-Price 4-D
+%   7 - Beale
+%   8 - Bukin02
+%   9 - Deckkers-Aarts
+%
+% Authors: Bohao Zhang and Shreyas Kousik
+% Created: Summer 2019
+% Updated: 10 Dec 2019
+%
+%% user parameters
+problem_index = 7; % pick an integer from 1 through 9
+
+%% optimal value analysis (automated from here)
 ground_truth = [0;0;-124.75;-24.77109375;0;0;0;0.000001712780354;0;0];
 total_steps = 20;
 step = 10;
@@ -10,30 +31,36 @@ Lasserre_result = infos.Lasserre_value_set  - ground_truth(problem_index+1);
 
 con_x = step:step:total_steps*step;
 
-figure
+figure(1) ; clf ;
 f_hd = boxplot(fmincon_result,'Labels',con_x);
 hold on;
-ba_hd = plot(1:total_steps,ba_result,'g*');
+ba_hd = plot(1:total_steps,ba_result,'bx','MarkerSize',9,'LineWidth',2);
 hold on;
-bsos_hd = plot(1:total_steps,Lasserre_result,'mo');
+bsos_hd = plot(1:total_steps,Lasserre_result,'ro','MarkerSize',10,'LineWidth',1.5);
 xlabel('number of constraints');
 ylabel('error');
 title('error vs. number of constraints');
+
+xtickangle(45)
+set(gca,'FontSize',14)
 
 %% time analysis
 ba_time       = infos.bernstein_time_set;
 fmincon_time  = infos.fmincon_time_set;
 Lasserre_time = infos.Lasserre_time_set;
 
-figure
+figure(2) ; clf ;
 f_hd = boxplot(log(fmincon_time),'Labels',con_x);
 hold on;
-ba_hd = plot(1:total_steps,log(ba_time),'g*');
+ba_hd = plot(1:total_steps,log(ba_time),'bx','MarkerSize',9,'LineWidth',2);
 hold on;
-bsos_hd = plot(1:total_steps,log(Lasserre_time),'mo');
+bsos_hd = plot(1:total_steps,log(Lasserre_time),'ro','MarkerSize',10,'LineWidth',1.5);
 upp = max(log(Lasserre_time));
 loww = log(min(min(min(fmincon_time)),min(ba_time)));
 axis([0 total_steps+1 loww upp+0.5]);
 xlabel('number of constraints');
 ylabel('solve time log(s)');
-title('time vs. number of constraints');
+title('time vs. number of constraints') ;
+
+xtickangle(45)
+set(gca,'FontSize',14)
