a0e70ae79c363e08cf1318cb6e0ffa6ad4896c64
Shreyas Kousik <skousik@umich.edu>
2019-12-10 14:51:18 -0500

    updated plot style

--- data_analysis.m
+++ data_analysis.m
@@ -1,5 +1,5 @@
 %% optimal value analysis
-problem_index = 9;
+problem_index = 2 ; % 2 is good
 ground_truth = [0;0;-124.75;-24.77109375;0;0;0;0.000001712780354;0;0];
 total_steps = 20;
 step = 10;
@@ -10,30 +10,36 @@ Lasserre_result = infos.Lasserre_value_set  - ground_truth(problem_index+1);
 
 con_x = step:step:total_steps*step;
 
-figure
+figure(1) ; clf ;
 f_hd = boxplot(fmincon_result,'Labels',con_x);
 hold on;
-ba_hd = plot(1:total_steps,ba_result,'g*');
+ba_hd = plot(1:total_steps,ba_result,'bx','MarkerSize',9,'LineWidth',2);
 hold on;
-bsos_hd = plot(1:total_steps,Lasserre_result,'mo');
+bsos_hd = plot(1:total_steps,Lasserre_result,'ro','MarkerSize',10,'LineWidth',1.5);
 xlabel('number of constraints');
 ylabel('error');
 title('error vs. number of constraints');
+
+xtickangle(45)
+set(gca,'FontSize',14)
 
 %% time analysis
 ba_time       = infos.bernstein_time_set;
 fmincon_time  = infos.fmincon_time_set;
 Lasserre_time = infos.Lasserre_time_set;
 
-figure
+figure(2) ; clf ;
 f_hd = boxplot(log(fmincon_time),'Labels',con_x);
 hold on;
-ba_hd = plot(1:total_steps,log(ba_time),'g*');
+ba_hd = plot(1:total_steps,log(ba_time),'bx','MarkerSize',9,'LineWidth',2);
 hold on;
-bsos_hd = plot(1:total_steps,log(Lasserre_time),'mo');
+bsos_hd = plot(1:total_steps,log(Lasserre_time),'ro','MarkerSize',10,'LineWidth',1.5);
 upp = max(log(Lasserre_time));
 loww = log(min(min(min(fmincon_time)),min(ba_time)));
 axis([0 total_steps+1 loww upp+0.5]);
 xlabel('number of constraints');
 ylabel('solve time log(s)');
-title('time vs. number of constraints');
+title('time vs. number of constraints') ;
+
+xtickangle(45)
+set(gca,'FontSize',14)
