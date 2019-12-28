8ff8b726e8fcc2c019bbb3e6cf7eef0dc873802d
Cfather <41474684+Cfather@users.noreply.github.com>
2019-12-27 22:29:37 -0500

    Add files via upload

---
+++ helper_functions/evaluate_opt_result.m
@@ -0,0 +1,23 @@
+function [value,feasibility,violate_terms,difference] = evaluate_opt_result(cost,constraints,input)
+% evaluate the value and the feasibility of the result
+violate_terms = [];
+difference = [];
+feasibility = 1;
+for i = 1:length(constraints)
+    cons_value = evaluate_function(cell2mat(constraints(i)),input);
+    if cons_value > 1 || cons_value < 0
+        feasibility = 0;
+        violate_terms = [violate_terms,i];
+        if(cons_value > 1)
+            difference = [difference,cons_value-1];
+        else
+            difference = [difference,cons_value];
+        end
+    end
+end
+if feasibility == 1
+    value = evaluate_function(cost,input);
+else
+    value = NaN;
+end
+end
