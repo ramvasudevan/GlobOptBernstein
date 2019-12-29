ccbdac4fe5aec4917c0fad8e1c4c8f38716f8de6
Shreyas Kousik <skousik@umich.edu>
2019-12-29 10:36:14 -0500

    added memory axis plop

--- Benchmark_Evaluation/time_and_memory_analysis/plot_time_and_memory_analysis.m
+++ Benchmark_Evaluation/time_and_memory_analysis/plot_time_and_memory_analysis.m
@@ -19,16 +19,24 @@ save_pdf_flag = true ;
 load(['P',num2str(problem_index),'_time_and_memory_info.mat'])
 
 %% process data
-% get the problem dimension
-d = size(bernstein_cost,1) - 1 ;
+% get the problem dimension and degree in each dimension ;
+dimension = size(bernstein_cost,1) - 1 ;
+degrees = max([bernstein_cost(1:dimension,:),bernstein_constraint(1:dimension,:)],[],2) ;
+
+% relabel the "bernstein memory" variable
+bernstein_N_patches = bernstein_memory ;
+
+% (over)approximate the memory used
+memory_per_patch = prod(degrees + 1) * 4 ; % 4 bytes per float (single)
+bernstein_memory = memory_per_patch.*bernstein_N_patches./1024 ; % in kilobytes
 
 % get the number of iterations (each entry in bernstein_memory is the
 % number of patches either from subdivision or elimination, and subdivision
 % happens once along each dimension in each iteration, so the denominator
 % is 2*d; the first "0" entry in bernstein_memory marks the iteration
 % where the problem was solved to the specified tolerances)
-problem_solved_index = find(bernstein_memory == 0,1,'first') + 1 ;
-num_iter = (problem_solved_index) / (2*d) ;
+problem_solved_index = find(bernstein_N_patches == 0,1,'first') + 1 ;
+num_iter = (problem_solved_index) / (2*dimension) ;
 
 % get indices for all steps
 all_indices = 1:problem_solved_index ;
@@ -41,30 +49,43 @@ subd_indices = all_indices ;
 subd_indices(elim_indices) = [] ;
 
 % get x values for plotting the number of iterations
-plot_subd_values = subd_indices ./ (2*d) ;
-plot_elim_values = elim_indices ./ (2*d) ;
+plot_subd_values = subd_indices ./ (2*dimension) ;
+plot_elim_values = elim_indices ./ (2*dimension) ;
 
 %% plot
 close all ; 
 f1 = figure(1) ; clf ; hold on ;
 
 % plot the number of patches at every subdivision step
-h_subd = plot(plot_subd_values,bernstein_memory(subd_indices),'b.','MarkerSize',12) ;
+h_subd = plot(plot_subd_values,bernstein_N_patches(subd_indices),'b.','MarkerSize',12) ;
 
 % plot the number of patches at each elimination step
-h_elim = plot(plot_elim_values,bernstein_memory(elim_indices),'r.','MarkerSize',12) ;
+h_elim = plot(plot_elim_values,bernstein_N_patches(elim_indices),'r.','MarkerSize',12) ;
+
+% label left side
+ylabel('Number of Patches')
 
 % add x ticks for every iteration
+grid on
 xticks(1:2:num_iter)
 xtickangle(45)
+
+% get the yticks
+yt_left = yticks ;
+
+% plot memory usage on right
+yyaxis right
+% set(gca,'LineColor','k')
+y_left_max = max(yt_left)*memory_per_patch./1024 ;
+h_mem = plot([0 1],[0,y_left_max],'LineStyle', 'none') ; % invisible plop
+ylabel('Approximate Memory Used [kB]')
+set(gca,'Color','none')
 
 % add legend
 legend([h_subd h_elim],'Subdivision','Elimination')
 
 % label plot
 xlabel('Iteration')
-ylabel('Number of Patches')
-grid on
 set(gca,'FontSize',14)
 
 % set plot size
