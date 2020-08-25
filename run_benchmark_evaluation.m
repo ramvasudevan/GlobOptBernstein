3a50e9ef32c73184307c147294533e0b880be5a7
Shreyas Kousik <skousik@umich.edu>
2020-08-25 11:31:33 -0400

    Revert "new data for DIRECT"
    This reverts commit a06b57ab86a94e67fbb5bb0a299f400d746a1059.

--- run_benchmark_evaluation.m
+++ run_benchmark_evaluation.m
@@ -7,16 +7,10 @@
 
 %% setup the problem
 clear all; clc;
-problem_index = 2;
+load Benchmark_Evaluation/PCBA_benchmark_optimal_solutions.mat
+problem_index = 3;
 % the true optimal value (or the best we found) for each problem
-ground_truth = [-5.5080132636,...
-                -6961.8138816446,...
-                3.0000011115,...
-                -4,...
-                0,...
-                6395.5078,...
-                1.0898639714,...
-                42.4440570797];
+ground_truth = optimal_value{problem_index};
 eval(strcat('[raw_cost,raw_constraints] = setup_problem_matrix_P',num2str(problem_index),'();'));
 numDimension = size(raw_cost,2) - 1;
 
@@ -168,22 +162,22 @@ save(strcat('Benchmark_Evaluation/P',num2str(problem_index),'_infos.mat'),'infos
 disp('PCBA time median:')
 disp(median(infos.bernstein_time_set));
 disp('PCBA error:')
-disp(infos.bernstein_value - ground_truth(problem_index));
+disp(infos.bernstein_value - ground_truth);
 disp('PCBA stopping crtieria:')
 disp(infos.bernstein_accuracy);
 disp(' ')
 disp('BSOS time median:')
 disp(median(infos.Lasserre_time_set));
 disp('BSOS error:')
-disp(infos.Lasserre_value - ground_truth(problem_index));
+disp(infos.Lasserre_value - ground_truth);
 disp('BSOS d & k choice:')
 disp([infos.Lasserre_d,infos.Lasserre_k]);
 disp(' ')
 disp('fmincon time median:')
 disp(median(infos.fmincon_time_set));
 disp('fmincon error median:')
-disp(median(infos.fmincon_value_set) - ground_truth(problem_index));
+disp(median(infos.fmincon_value_set) - ground_truth);
 disp('DIRECT time median:')
 disp(median(infos.DIRECT_time_set));
 disp('DIRECT error median:')
-disp(median(infos.DIRECT_value) - ground_truth(problem_index));
+disp(median(infos.DIRECT_value) - ground_truth);
